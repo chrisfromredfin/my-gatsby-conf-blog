4cfef2f8a22ace65456df008b210cf907d719744
Christopher J. Wells <chris@redfinsolutions.com>
2021-03-03 13:26:54 -0500

    links

---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+const MyComponent = () => {
+  return (
+    <>
+      <h1>Hi, welcome to my site!</h1>
+      <Link to='/'>Go Home!</Link>
+    </>
+  );
+};
+
+export default MyComponent;
